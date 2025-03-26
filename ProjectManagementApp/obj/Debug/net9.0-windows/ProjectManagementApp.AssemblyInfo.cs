0b5152bc066ffdb8074df7b8f4206713e244950b
Owen <Ahong9401@gmail.com>
2025-03-26 22:51:36 +0700

    "Update project"

--- ProjectManagementApp/obj/Debug/net9.0-windows/ProjectManagementApp.AssemblyInfo.cs
+++ ProjectManagementApp/obj/Debug/net9.0-windows/ProjectManagementApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManagementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc977cdafe46687ff8582bf6326bd8e75de44b22")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManagementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManagementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
